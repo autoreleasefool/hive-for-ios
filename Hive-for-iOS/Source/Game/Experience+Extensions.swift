eb12222f71e3ed9ccbf1e74cbf4275626a74ae6f
Joseph Roque <contact@josephroque.ca>
2020-01-28 20:59:57 +0100

    Convenience functions to interop w/ experience

---
+++ Hive-for-iOS/Source/Game/Experience+Extensions.swift
@@ -0,0 +1,64 @@
+//
+//  Experience+Extensions.swift
+//  Hive-for-iOS
+//
+//  Created by Joseph Roque on 2020-01-27.
+//  Copyright © 2020 Joseph Roque. All rights reserved.
+//
+
+import RealityKit
+import HiveEngine
+
+extension Experience.HiveGame {
+	var allPieces: [Entity?] {
+		return blackPieces + whitePieces
+	}
+
+	var blackPieces: [Entity?] {
+		return [
+			bA1,
+			bA2,
+			bA3,
+			bB1,
+			bB2,
+			bG1,
+			bG2,
+			bG3,
+			bL,
+			bM,
+			bP,
+			bQ,
+			bS1,
+			bS2,
+		]
+	}
+
+	var whitePieces: [Entity?] {
+		return [
+			wA1,
+			wA2,
+			wA3,
+			wB1,
+			wB2,
+			wG1,
+			wG2,
+			wG3,
+			wL,
+			wM,
+			wP,
+			wQ,
+			wS1,
+			wS2,
+		]
+	}
+}
+
+extension Entity {
+	func visit(using block: (Entity) -> Void) {
+		block(self)
+
+		for child in children {
+			child.visit(using: block)
+		}
+	}
+}
