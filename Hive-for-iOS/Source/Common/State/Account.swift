070e142a1853e4448650c565555d1bc1afb8feca
Joseph Roque <contact@josephroque.ca>
2020-04-04 14:38:44 +0200

    Use default account for debugging

--- Hive-for-iOS/Source/Common/State/Account.swift
+++ Hive-for-iOS/Source/Common/State/Account.swift
@@ -11,26 +11,31 @@ import Combine
 import KeychainAccess
 
 class Account: ObservableObject {
+	private static let debugAccountEnabled: Bool = true
+
 	private enum Key: String {
 		case userId
-		case accessToken
+		case token
 	}
 
 	private(set) var userId: User.ID?
-	private(set) var accessToken: String?
+	private(set) var token: String?
 	@Published var isAuthenticated: Bool = false
 
 	private let keychain = Keychain(service: "ca.josephroque.hive-for-ios")
 
 	init() {
-		try? clear()
+		if Account.debugAccountEnabled {
+			try? store(userId: UUID(uuidString: "60448917-d472-4099-b1c8-956935245d6e"))
+			try? store(token: "w6d9J8nap70BhiB63ZTyAQ==")
+		}
 
 		do {
 			guard let id = try keychain.get(Key.userId.rawValue) else { return }
-			guard let token = try keychain.get(Key.accessToken.rawValue) else { return }
+			guard let token = try keychain.get(Key.token.rawValue) else { return }
 
 			userId = UUID(uuidString: id)
-			accessToken = token
+			self.token = token
 			isAuthenticated = true
 		} catch {
 			print("Error retrieving login: \(error)")
@@ -39,13 +44,13 @@ class Account: ObservableObject {
 
 	func clear() throws {
 		try store(userId: nil)
-		try store(accessToken: nil)
+		try store(token: nil)
 		isAuthenticated = false
 	}
 
 	func store(accessToken: AccessToken) throws {
 		try store(userId: accessToken.userId)
-		try store(accessToken: accessToken.token)
+		try store(token: accessToken.token)
 		isAuthenticated = true
 	}
 
@@ -57,11 +62,11 @@ class Account: ObservableObject {
 		}
 	}
 
-	private func store(accessToken: String?) throws {
-		if let accessToken = accessToken {
-			try keychain.set(accessToken, key: Key.accessToken.rawValue)
+	private func store(token: String?) throws {
+		if let token = token {
+			try keychain.set(token, key: Key.token.rawValue)
 		} else {
-			try keychain.remove(Key.accessToken.rawValue)
+			try keychain.remove(Key.token.rawValue)
 		}
 	}
 }
