7f5b80a152281ec53340b42433c5731cc4d87ad0
Joseph Roque <github@josephroque.ca>
2021-04-17 10:55:35 -0400

    Blank watch app

---
+++ Hive-for-watchOS-Extension/Source/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Hive-for-watchOS-Extension
+//
+//  Created by Joseph Roque on 2021-04-17.
+//  Copyright © 2021 Joseph Roque. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+	var body: some View {
+		Text("Hello, World!")
+			.padding()
+	}
+}
+
+struct ContentView_Previews: PreviewProvider {
+	static var previews: some View {
+		ContentView()
+	}
+}
---
+++ Hive-for-watchOS-Extension/Source/HiveWatchApp.swift
@@ -0,0 +1,20 @@
+//
+//  HiveWatchApp.swift
+//  Hive-for-watchOS-Extension
+//
+//  Created by Joseph Roque on 2021-04-17.
+//  Copyright © 2021 Joseph Roque. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct HiveWatchApp: App {
+	var body: some Scene {
+		WindowGroup {
+			NavigationView {
+				ContentView()
+			}
+		}
+	}
+}
